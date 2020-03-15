a8b3c7e308b5c21562f0aa21af8bdc89fcf39a38
Josh Veitch-Michaelis <j.veitchmichaelis@gmail.com>
2020-03-15 21:50:18 +0000

    use fixtures

--- test/test_leds.py
+++ test/test_leds.py
@@ -1,35 +1,44 @@
 from pca9632.controller import Controller, REG_MODE1, REG_PWM0, REG_PWM1, REG_PWM2, REG_PWM3
-import time
+import pytest
 
-def test_reset():
+@pytest.fixture
+def led_controller():
     dev = Controller()
+
+    yield dev
+
+    dev.reset()
+    dev.all_off()
+    dev.power_off()
+
+def test_reset(led_controller):
+    dev = led_controller
     dev.reset()
 
-def test_power_on():
-    dev = Controller()
+def test_power_on(led_controller):
+    dev = led_controller
     dev.power_on()
     assert dev._read(REG_MODE1) >> 4 & 1 == 0
 
-def test_power_off():
-    dev = Controller()
+def test_power_off(led_controller):
+    dev = led_controller
     dev.power_off()
     assert dev._read(REG_MODE1) >> 4 & 1 == 1
 
-def test_enable():
-    dev = Controller()
+def test_enable(led_controller):
+    dev = led_controller
     for i in range(4):
         dev.led_enable(i)
         assert dev.led_enabled(i) == True
 
-def test_disable():
-    dev = Controller()
+def test_disable(led_controller):
+    dev = led_controller
     for i in range(4):
         dev.led_disable(i)
         assert dev.led_enabled(i) == False
 
-def test_all_on():
-    dev = Controller()
-    dev.power_on()
+def test_all_on(led_controller):
+    dev = led_controller
     dev.all_on()
     
     assert dev._read(REG_PWM0) == 255
@@ -37,24 +46,16 @@ def test_all_on():
     assert dev._read(REG_PWM2) == 255
     assert dev._read(REG_PWM3) == 255
 
-    dev.all_off()
-    dev.power_off()
-
 def test_all_off():
-    dev = Controller()
-    dev.power_on()
-    
+    dev = Controller() 
     dev.all_off()
     dev.power_off()
 
-def test_full_range():
-    dev = Controller()
-    dev.power_on()
+def test_full_range(led_controller):
+    dev = led_controller
 
     for i in range(4):
         for v in range(256):
             dev.set_led(i, v)
             assert v == dev._read(REG_PWM0+i)
  
-    dev.all_off()
-    dev.power_off()
